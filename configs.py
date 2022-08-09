ba4c3da5be62cb73b9ece6046fc193eab6e828d5
akorol98 <akorol98.17@gmail.com>
2022-08-09 15:30:56 +0200

    GAN multiple discriminators

--- configs.py
+++ configs.py
@@ -3,31 +3,33 @@ class Configs():
     def __init__(self):
         
     # Experiment Name
-        self.name = '4channels_seeded_sGAN'
+        self.name = '4channels_seeded_GAN_34992_len'
 
     # Generator
         self.nz = 100
-        self.noise = 'uniform'
+        self.noise = 'normal'
         # labels for training
         self.Emax = 1
         self.Emin = 1
 
     # Discriminator
+        self.num_discriminators = 10
         self.nfd = 32
 
     # Dtataloader
         self.dataPath = '/beegfs/desy/user/akorol/projects/getting_high/ILDConfig/StandardConfig/production/out/no_projection.hdf5'
-        self.workers = 64
+        self.workers = 128
         self.bs = 32
         self.pin_memory = True
         self.shuffle = True
-        self.fake2realRatio = 3     
 
     # Optimizer
         self.n_epochs = 200
-        self.lr = 0.0001
+        self.lrG = 0.000005
+        self.lrD = 0.000001
         self.beta1 = 0.5
-        self.weight_decayD = 0.1
+        self.beta2 = 0.999
+        self.weight_decayD = 0
         self.weight_decayG = 0
         self.generator_loss = 'BCE'
         self.gLossFactor = 1
@@ -36,7 +38,7 @@ class Configs():
 
     # Others
         self.device = 'cuda'
-        # self.checkpoint = '/beegfs/desy/user/akorol/trained_models/overfitting_bs1024_lr0.0001Gx1_LossMSE/epoch_20000.pth'
+        # self.checkpoint = '/beegfs/desy/user/akorol/trained_models/4channels_seeded_sGAN_WU-False_bs32_lr0.0001Gx1_L2RD0.1_L2RG0_LossBCE_gamma25_uniformNoise_fake2realRatio1/epoch_200.pth'
         self.checkpoint = None
         self.wormup = False
         self.path2save = '/beegfs/desy/user/akorol/trained_models'
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from models import GNet, DNet
 from dataset import HDF5Dataset
 from configs import Configs
+from models.utils import weights_init
 
 # import matplotlib.pyplot as plt
 import random
@@ -20,7 +21,7 @@ g.manual_seed(cfg.seed)
 device = torch.device(cfg.device)
 
 
-def save(netG, netD, omtim_G, optim_D, epoch, loss, scores, z, path_to_save):
+def save(netG, netD, omtim_G, optim_D, epoch, loss, scores, path_to_save):
     torch.save({
                 'Generator': netG.state_dict(),
                 'Discriminator': netD.state_dict(),
@@ -33,16 +34,24 @@ def save(netG, netD, omtim_G, optim_D, epoch, loss, scores, z, path_to_save):
                 },
                 path_to_save)
 
+def get_gen_seed(bs, noise=cfg.noise):
+    if noise == 'uniform':
+        z = torch.FloatTensor(int(bs), cfg.nz, 1).uniform_(-1, 1).to(device)
+    else:
+        z = torch.FloatTensor(int(bs), cfg.nz, 1).normal_(0, 1).to(device)
+    e_labels = torch.FloatTensor(bs, 1, 1).uniform_(cfg.Emax, cfg.Emin).to(device)
 
-print(f'workers: {cfg.workers}, bs: {cfg.bs}, beta1: {cfg.beta1}, lr: {cfg.lr}, pin_memory: {cfg.pin_memory}, netG loss: {cfg.generator_loss}, gLossFactor: {cfg.gLossFactor}')
+    return z, e_labels
+
+
 print(f'checkpoint: {cfg.checkpoint}')
-train_folder = f'{cfg.path2save}/{cfg.name}_WU-{cfg.wormup}_bs{cfg.bs}_lr{cfg.lr}Gx{cfg.gLossFactor}_L2RD{cfg.weight_decayD}_L2RG{cfg.weight_decayG}_Loss{cfg.generator_loss}_gamma{cfg.gamma}_{cfg.noise}Noise_fake2realRatio{cfg.fake2realRatio}'
+train_folder = f'{cfg.path2save}/{cfg.name}/WU-{cfg.wormup}_bs{cfg.bs}_lr{cfg.lrD}G{cfg.lrG}_L2RD{cfg.weight_decayD}_L2RG{cfg.weight_decayG}_Loss{cfg.generator_loss}_gamma{cfg.gamma}_{cfg.noise}Noise_num_discriminators{cfg.num_discriminators}'
 if not os.path.exists(train_folder):
     os.makedirs(train_folder)
 print(f'train_folder: {train_folder}')
 
 with open(f'{train_folder}/log.txt', "w") as file:
-        file.write(f'[CONFIGS] workers: {cfg.workers}, bs: {cfg.bs}, beta1: {cfg.beta1}, lr: {cfg.lr}, pin_memory: {cfg.pin_memory}, netG loss: {cfg.generator_loss}, gLossFactor: {cfg.gLossFactor}\n')
+        file.write(f'[CONFIGS] workers: {cfg.workers}, bs: {cfg.bs}, beta1: {cfg.beta1}, lr{cfg.lrD}G{cfg.lrG}, pin_memory: {cfg.pin_memory}, netG loss: {cfg.generator_loss}, gLossFactor: {cfg.gLossFactor}\n')
         file.write(f'checkpoint: {cfg.checkpoint}\n')
 
 
@@ -55,17 +64,23 @@ dataloader = torch.utils.data.DataLoader(data, batch_size=cfg.bs, pin_memory = c
 
 
 
-netG = GNet.GeneratorS(cfg)
-netG.apply(GNet.weights_init)
+netG = GNet.Generator(cfg)
+netG.apply(weights_init)
 netG.to(device)
     
-netD = DNet.DiscriminatorS(cfg)
-netD.apply(GNet.weights_init)
-netD.to(device)
+D_nets = []
+D_optimizers = []
+for i in range(cfg.num_discriminators):
+    netD = DNet.Discriminator(cfg)
+    netD.apply(weights_init)
+    netD.to(device)
+    optimizer_D = torch.optim.Adam(netD.parameters(), lr=cfg.lrD, betas=(cfg.beta1, 0.999), weight_decay=cfg.weight_decayD)
+    D_nets.append(netD)
+    D_optimizers.append(optimizer_D)
 
 
-optimizer_G = torch.optim.Adam(netG.parameters(), lr=cfg.lr*cfg.gLossFactor, betas=(cfg.beta1, 0.999), weight_decay=cfg.weight_decayG)
-optimizer_D = torch.optim.Adam(netD.parameters(), lr=cfg.lr, betas=(cfg.beta1, 0.999), weight_decay=cfg.weight_decayD)
+optimizer_G = torch.optim.Adam(netG.parameters(), lr=cfg.lrG, betas=(cfg.beta1, 0.999), weight_decay=cfg.weight_decayG)
+
 criterion = nn.BCEWithLogitsLoss()
 MSEcriterion = nn.MSELoss()
 
@@ -91,8 +106,8 @@ if cfg.checkpoint:
         optimizer_D.load_state_dict(checkpoint['D_optimizer'])
         eph = checkpoint['epoch']
         chechoint_loss = checkpoint['loss']
-        G_losses = chechoint_loss[0]
-        D_losses = chechoint_loss[1]
+        G_losses = chechoint_loss['G_losses']
+        D_losses = chechoint_loss['D_losses']
         chechoint_scores = checkpoint['D_scores']
         D_scores_x = chechoint_scores[0]
         D_scores_z1 = chechoint_scores[1]
@@ -109,79 +124,76 @@ if cfg.checkpoint:
 
 
 
-def train(real_showers, netG, netD, epoch, noise=cfg.noise):
+def train(real_showers, netG, D_nets, D_optimizers, epoch, noise=cfg.noise):
     bs = len(real_showers)
 
     # Adversarial ground truths
     valid_label = torch.FloatTensor(np.ones(bs)).to(device)
-    fake_label = torch.FloatTensor(np.zeros(int(bs*cfg.fake2realRatio))).to(device)
+    fake_label = torch.FloatTensor(np.zeros(int(bs))).to(device)
 
     ######################################################
     # Train Discriminator
     ######################################################
-    netD.zero_grad()
+    errD_mean = 0
+    for netD, optimizer_D in list(zip(D_nets, D_optimizers)):
+        netD.zero_grad()
 
-    output = netD(real_showers).view(-1)
-    errD_real = criterion(output, valid_label)
-    errD_real.backward()
-    D_x = output.mean().item()
+        output = netD(real_showers).view(-1)
+        errD_real = criterion(output, valid_label)
+        errD_real.backward()
+        D_x = output.mean().item()
 
-    if noise == 'uniform':
-        z = torch.FloatTensor(int(bs*cfg.fake2realRatio), cfg.nz, 1).uniform_(-1, 1).to(device)
-    else:
-        z = torch.FloatTensor(int(bs*cfg.fake2realRatio), cfg.nz, 1).normal_(0, 1).to(device)
+        z, e_labels = get_gen_seed(bs)
+        fake_shower = netG(z, e_labels)
 
-    e_labels = torch.FloatTensor(bs*cfg.fake2realRatio, 1, 1).uniform_(cfg.Emax, cfg.Emin).to(device)
-    fake_shower = netG(z, e_labels)
+        output = netD(fake_shower).view(-1)
+        errD_fake = criterion(output, fake_label)
+        errD_fake.backward(retain_graph=True)
+        D_G_z1 = output.mean().item()
+        errD = (errD_real.item() + errD_fake.item())/2
+        
+        errD_mean += errD / len(D_nets)
 
-    output = netD(fake_shower).view(-1)
-    errD_fake = criterion(output, fake_label)
-    errD_fake.backward(retain_graph=True)
-    D_G_z1 = output.mean().item()
-    errD = errD_real + errD_fake
-
-    optimizer_D.step()
+        optimizer_D.step()
 
     if cfg.wormup:
         if epoch == 1:
-            errG_total = errD
             D_G_z2 = D_G_z1
 
-            errG_MSE = errD_real
             errG = errD_real
 
-            return errG_total, errD, D_x, D_G_z1, D_G_z2, errG_MSE, errG
+            return errD_mean, D_x, D_G_z1, D_G_z2, errG
 
     ######################################################
     # Train Generator
     ######################################################
     netG.zero_grad()
 
-    if cfg.generator_loss == "MSE+BCE":
-        errG_MSE = MSEcriterion(fake_shower[:len(real_showers)], real_showers)
-        
-        output = netD(fake_shower).view(-1)
-        valid_label = torch.FloatTensor(np.ones(len(output))).to(device)
-        errG = criterion(output, valid_label)
-        D_G_z2 = output.mean().item()
+    if cfg.generator_loss == "MSE":
 
-        errG_total = errG + errG_MSE*cfg.gamma
+        embeding_fake = netD.MainConv(fake_shower)
+        embeding_real = netD.MainConv(real_showers)
+
+        errG_total = MSEcriterion(embeding_fake, embeding_real)
+        
+        D_G_z2 = 0
 
         errG_total.backward(retain_graph=True)
         optimizer_G.step()
     
     else:
-        output = netD(fake_shower).view(-1)
-        valid_label = torch.FloatTensor(np.ones(len(output))).to(device)
-        errG = criterion(output, valid_label)
-        errG.backward(retain_graph=True)
+        errG_total = 0
+        for netD in (D_nets):
+            z, e_labels = get_gen_seed(bs)
+            fake_shower = netG(z, e_labels)
+            output = netD(fake_shower).view(-1)
+            valid_label = torch.FloatTensor(np.ones(len(output))).to(device)
+            errG_total += criterion(output, valid_label)
+        errG_total.backward(retain_graph=True)
         D_G_z2 = output.mean().item()
         optimizer_G.step()
 
-        errG_total = errG
-        errG_MSE = errG
-
-    return errG_total, errD, D_x, D_G_z1, D_G_z2, errG_MSE, errG
+    return errD_mean, D_x, D_G_z1, D_G_z2, errG_total.item()
 
 
 
@@ -195,35 +207,32 @@ if __name__ == '__main__':
             real_showers = batch['event'].float().to(device)
     #         real_energys = batch['energy'].float().to(device)
 
-            errG_total, errD, D_x, D_G_z1, D_G_z2, errG_MSE, errG = train(real_showers, netG, netD, epoch)
+            errD, D_x, D_G_z1, D_G_z2, errG = train(real_showers, netG, D_nets, D_optimizers, epoch)
 
             # Output training stats
-            G_losses = np.append(G_losses, errG_total.item())
-            D_losses = np.append(D_losses, errD.item())
+            G_losses = np.append(G_losses, errG)
+            D_losses = np.append(D_losses, errD)
             D_scores_x = np.append(D_scores_x, D_x)
             D_scores_z1 = np.append(D_scores_z1, D_G_z1)
             D_scores_z2 = np.append(D_scores_z2, D_G_z2)
-            G_loss_MSE = np.append(G_loss_MSE, errG_MSE.item())
-            G_loss_BCE = np.append(G_loss_BCE, errG.item())
 
 
         t = round(time() - time_start)
         t_per_sample = round(t/len(dataloader)/cfg.bs, 5)
         print(f'Epoch {epoch} finished, time: {t} s / epoch; {t_per_sample} s / sample')
         print('[%d/%d], (Loss_D: %.4f)  (Loss_G: %.4f),  (D(x): %.4f)  (D(G(z)): %.4f / %.4f)  \n\n\n'
-                    % (epoch, cfg.n_epochs, errD.item(), errG_total.item(), D_x, D_G_z1, D_G_z2))
+                    % (epoch, cfg.n_epochs, errD, errG, D_x, D_G_z1, D_G_z2))
         with open(f'{train_folder}/log.txt', "a") as file:
             file.write(f'[INFO] time: {t} s / epoch; {t_per_sample} s / sample\n')
             file.write('[INFO] Epoch [%d/%d], (Loss_D: %.4f)  (Loss_G: %.4f),  (D(x): %.4f)  (D(G(z)): %.4f / %.4f)  \n\n'
-                    % (epoch, cfg.n_epochs, errD.item(), errG_total.item(), D_x, D_G_z1, D_G_z2))
+                    % (epoch, cfg.n_epochs, errD, errG, D_x, D_G_z1, D_G_z2))
 
 
         if epoch%5 == 0:
             PATH_save = f'{train_folder}/epoch_{epoch}.pth'
 
             # loss =  np.array([G_losses, D_losses])
-            GL = {'Total' : G_losses, 'MSE': G_loss_MSE, 'BCE': G_loss_BCE}
-            loss = {'G_losses': GL, 'D_losses': D_losses}
+            loss = {'G_losses': G_losses, 'D_losses': D_losses}
             D_scores = np.array([D_scores_x, D_scores_z1, D_scores_z2])
 
             save(netG=netG, netD=netD, omtim_G=optimizer_G, optim_D=optimizer_D, epoch=epoch, loss=loss, scores=D_scores,
